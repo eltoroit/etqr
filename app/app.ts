cdf3a0eaf962f9766838564f7eca713cf0662b56
Andres Perez <andresperezf@yahoo.com>
2019-12-13 13:16:15 -0500

    Moved code

--- app/app.ts
+++ app/app.ts
@@ -51,14 +51,14 @@ app.listen(PORT, () => {
 	} else {
 		console.log('Heroku server started');
 	}
+});
 
-	app.use((req, res, next) => {
-		if (req.header('x-forwarded-proto') === 'https') {
-			// request was via https, so do no special handling
-			next();
-		} else {
-			// request was via http, so redirect to https
-			res.redirect('https://' + req.headers.host + req.url);
-		}
-	});
+app.use((req, res, next) => {
+	if (req.header('x-forwarded-proto') === 'https') {
+		// request was via https, so do no special handling
+		next();
+	} else {
+		// request was via http, so redirect to https
+		res.redirect('https://' + req.headers.host + req.url);
+	}
 });
